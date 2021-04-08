fa499238df49eeaf5fbf1f6000f3fc366a6264c2
RahmanSabir <81110453+RahmanSabir@users.noreply.github.com>
2021-04-09 01:45:04 +0600

    new

--- index.php
+++ index.php
@@ -1,70 +1,73 @@
-<?php 
+<?php
 
 include "db_connection.php";
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
   <title>Book</title>
 </head>
+
 <body>
-<section class="nav justify-content-center">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="Register.html">Register</a>
-          </li>
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-              Catagories
-            </button>
-            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-<?php 
+  <section class="nav justify-content-center m-3 text-warning">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Register.html">Register</a>
+            </li>
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                Catagories
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                <?php
 
-$sql = "SELECT * FROM category";
-$result = mysqli_query($connect, $sql);
-$output ='';
-if (mysqli_num_rows($result) > 0 ) {
-	while ($row = mysqli_fetch_array($result)) {
-		echo '<li><a class="dropdown-item" href="#">'.$row['cat_title'].'</a></li>';
-	}
-}
+                $sql = "SELECT * FROM category";
+                $result = mysqli_query($connect, $sql);
+                $output = '';
+                if (mysqli_num_rows($result) > 0) {
+                  while ($row = mysqli_fetch_array($result)) {
+                    echo '<li><a class="dropdown-item" href="#">' . $row['cat_title'] . '</a></li>';
+                  }
+                }
 
- ?>
-            </ul>
-          </div>
-        </ul>
+                ?>
+              </ul>
+            </div>
+          </ul>
 
-        <form class="d-flex justify-content-end">
-          <input class="form-control me-2" type="search" placeholder="Search Books Here" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+          <form class="d-flex justify-content-end">
+            <input class="form-control me-2" type="search" placeholder="Search Books Here" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </div>
-    </div>
-    <li class="nav-item">
-      <a class="nav-link" href="cart.html">Cart</a>
-    </li>
-  </nav>
+      <li class="nav-item">
+        <a class="nav-link" href="cart.html">Cart</a>
+      </li>
+    </nav>
 
-  
+
 
   </section>
 
@@ -72,32 +75,25 @@ if (mysqli_num_rows($result) > 0 ) {
 
 
 
-  <section class="container ">
-    <div class="row">
-      <div class="col-md-3">
+  <section class="container m-5">
+    <div class="row  mt-sm-10">
+      <div class="col-md-6 ">
         About us
-      </div>
-      <div class="col-md-6">
-        login
+
+        <h6 class="m-2  ">We are online based book seller.You have your department based books. Do register nd be updated</h6>
       </div>
       <div class="col-md-3">
-        hg
+
+      </div>
+      <div class="col-md-3">
+        <a class="btn btn-primary btn-success" href="login.html" role="button">signIn</a>
       </div>
     </div>
   </section>
-    
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
