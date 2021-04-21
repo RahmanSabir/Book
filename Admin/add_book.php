669dcdcb13468125f06736834660b78298be15c1
RahmanSabir <81110453+RahmanSabir@users.noreply.github.com>
2021-04-21 14:40:41 +0600

    Add files via upload

---
+++ Admin/add_book.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <h2 class="text-center p-3 text-light bg-success mt-5">Adding Books</h2>
+      
+    <div id="page">
+	<!-- start content -->
+	<div id="content">
+		<div class="post" style="margin-left:100px">
+			<h1 class="title"></h1>
+			<div class="entry" style="min-height:400px">
+				<form  method='POST'>
+						<b style="color:darkgreen">ADD Book </b>
+							<br/>
+							<input type='text' name= 'cat' size='25' placeholder="book ad">
+<br>
+              
+							
+							<input type='submit'  value='  ADD  '>
+<?php
+
+//session_start();
+                  
+$connect = mysqli_connect("localhost", "root" );
+
+mysqli_select_db($connect, 'sabir');
+              if(!empty($_POST))
+	{
+		$msg=array();
+		if(empty($_POST['cat']))
+		{
+			$msg[]="Please full fill all requirement";
+		}
+		
+		if(!empty($msg))
+		{
+			echo '<b>Error:-</b><br>';
+			
+			foreach($msg as $k)
+			{
+				echo '<li>'.$k;
+			}
+		}
+		else
+		{
+	
+		
+			$cat=$_POST['cat'];
+
+      
+			
+			$query="insert into product (product_image) values('$cat')";
+			
+			mysqli_query($connect,$query) or die("can't Execute...");
+			
+			//mysql_close($link);
+		//	header("location:category.php");
+		}
+	}
+	else
+	{
+		
+	}
+		?>					
+							<br><br>
+				</form>
+				<hr>
+	<!-- start sidebar -->
+	
+	<!-- end sidebar -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end page -->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
