0bd5a6b917bb659fe411ea69222d16311c946c09
RahmanSabir <81110453+RahmanSabir@users.noreply.github.com>
2021-04-22 23:36:13 +0600

    CRUD

---
+++ Admin/deleteBook.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+session_start();
+                  
+$connect = mysqli_connect("localhost", "root" );
+
+mysqli_select_db($connect, 'sabir');
+
+$id = $_GET['id'];
+
+$q = " delete from product where cat_id=$id " ;
+
+mysqli_query($connect , $q);
+
+header ('location:display.php');
+
+?>
