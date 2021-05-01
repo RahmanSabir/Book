ff56e3198f75b532df60297ebaa7486077734bae
RahmanSabir <81110453+RahmanSabir@users.noreply.github.com>
2021-05-01 16:07:07 +0600

    showing data

--- wishlist.php
+++ wishlist.php
@@ -1,56 +1,81 @@
-<?php 
-  include('db_connect.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
-    <title>Wishlist</title>
-</head>
-<body>
-    
 <?php
-  $id = $_GET['id'];
-  $sql = "SELECT * FROM `product` WHERE product_id='{$id}'";
-  $result = mysqli_query($connect, $sql);
-  $data = mysqli_fetch_array($result);
-  //print_r($data);
- ?>
 
-    <section class="container m-5">
-        <div class="row">
-          <div class="col-md-6 ">
-           
-            <img class="img-fluid" src="img/<?php echo $data['product_image'] ?>" alt="Card image cap">
-    
-          </div>
-          <div class="col-md-6 mt-5">
-                <h6>name : <span class="text-secondary"><?php echo $data['product_name'] ?></span></h6>
-                <h6>price : <span class="text-secondary">$<?php echo $data['product_price'] ?></span> </h6>
-                <h6>Writer : <?php echo $data['writer'] ?></h6>
-                <h6>Edition : 8th</h6>
-                
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "sabir");
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Wishlist </title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<br />
+	<div class="container">
+		<br />
+		<br />
+		<br />
+		<h3 align="center">Shoping Cart With PHP And MySql </a></h3><br />
+		<br /><br />
+		<?php
+		$query = "SELECT * FROM product ORDER BY product_id ASC";
+		$result = mysqli_query($connect, $query);
+		if (mysqli_num_rows($result) > 0) {
+			while ($row = mysqli_fetch_array($result)) {
+		?>
+
+				<div class="col-md-4">
+					<form method="post" action="wishlist.php?action=add&id=<?php echo $row["product_id"]; ?>">
+						<div style="border:3px solid #5cb85c; background-color:whitesmoke; border-radius:5px; padding:16px;" align="center">
+							<img src="img/<?php echo $row["product_image"]; ?>" class="img-responsive" /><br />
+
+							<h4 class="text-info"><?php echo $row["product_name"]; ?></h4>
+
+							<h4 class="text-danger">$ <?php echo $row["product_price"]; ?></h4>
+
+							<input type="submit" name="add_to_wishlist" style="margin-top:5px;" class="btn btn-success" value="Add to Wishlist" />
+<?php
                  
-<a class="btn btn-primary btn-success" href="cart.html" role="button"><svg class="mr-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-</svg>Add to Wishlist</a> 
-  
-      <a class="btn btn-primary btn-success" href="wishlist.php" role="button">Add to cart</a>
-            </div>
-        </div>
-    </section>
+?>
+						</div>
+					</form>
+				</div>
+		<?php
+			}
+		}
+		?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		<div style="clear:both"></div>
+		<br />
+		<h3>Order Details</h3>
+		<div class="table-responsive">
+			<table class="table table-bordered">
+				<tr>
+					<th width="40%">Product Name</th>
+
+					<th width="20%">Price</th>
+					
+					<th width="5%">Action</th>
+				</tr>
+
+
+
+			</table>
+
+			<div class="col-sm-12 col-md-6 text-right">
+					<a href="user.php" class="btn btn-md btn-block btn-success text-uppercase"> Back </a>
+					</div>
+			</div>
+	</div>
+	
 </body>
 </html>
