df4d1fcaef1e8d8ecbdd8e56313f58f83c68f84e
RahmanSabir <81110453+RahmanSabir@users.noreply.github.com>
2021-05-02 01:00:44 +0600

    add remove

--- wishlist.php
+++ wishlist.php
@@ -1,81 +1,154 @@
-<?php
+<?php 
 
 session_start();
 $connect = mysqli_connect("localhost", "root", "", "sabir");
 
+if(isset($_POST["add_to_cart"]))
+{
+	if(isset($_SESSION["shopping_cart"]))
+	{
+		$item_array_id = array_column($_SESSION["shopping_cart"], "product_id");
+		if(!in_array($_GET["id"], $item_array_id))
+		{
+			$count = count($_SESSION["shopping_cart"]);
+			$item_array = array(
+				'product_id'			=>	$_GET["id"],
+				'product_name'			=>	$_POST["hidden_name"],
+				'product_price'		=>	$_POST["hidden_price"],
+				
+			);
+			$_SESSION["shopping_cart"][$count] = $item_array;
+		}
+		else
+		{
+			echo '<script>alert("Item Already Added")</script>';
+		}
+	}
+	else
+	{
+		$item_array = array(
+			'product_id'			=>	$_GET["id"],
+			'product_name'			=>	$_POST["hidden_name"],
+			'product_price'		=>	$_POST["hidden_price"],
+		
+		);
+		$_SESSION["shopping_cart"][0] = $item_array;
+	}
+}
 
-
-
+if(isset($_GET["action"]))
+{
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["shopping_cart"] as $keys => $values)
+		{
+			if($values["product_id"] == $_GET["id"])
+			{
+				unset($_SESSION["shopping_cart"][$keys]);
+				echo '<script>alert("Item Removed")</script>';
+				echo '<script>window.location="wishlist.php"</script>';
+			}
+		}
+	}
+}
 
 ?>
 <!DOCTYPE html>
 <html>
-
-<head>
-	<title>Wishlist </title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-	<br />
-	<div class="container">
+	<head>
+		<title>Cart </title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
 		<br />
-		<br />
-		<br />
-		<h3 align="center">Shoping Cart With PHP And MySql </a></h3><br />
-		<br /><br />
-		<?php
-		$query = "SELECT * FROM product ORDER BY product_id ASC";
-		$result = mysqli_query($connect, $query);
-		if (mysqli_num_rows($result) > 0) {
-			while ($row = mysqli_fetch_array($result)) {
-		?>
+		<div class="container">
+			<br />
+			<br />
+			<br />
+			<h3 align="center">Shoping Cart With PHP And MySql </a></h3><br />
+			<br /><br />
+			<?php
+				$query = "SELECT * FROM product ORDER BY product_id ASC";
+				$result = mysqli_query($connect, $query);
+				if(mysqli_num_rows($result) > 0)
+				{
+					while($row = mysqli_fetch_array($result))
+					{
+				?>
+			<div class="col-md-4">
+				<form method="post" action="wishlist.php?action=add&id=<?php echo $row["product_id"]; ?>">
+					<div style="border:3px solid #5cb85c; background-color:whitesmoke; border-radius:5px; padding:16px;" align="center">
+						<img src="img/<?php echo $row["product_image"]; ?>" class="img-responsive" /><br />
 
-				<div class="col-md-4">
-					<form method="post" action="wishlist.php?action=add&id=<?php echo $row["product_id"]; ?>">
-						<div style="border:3px solid #5cb85c; background-color:whitesmoke; border-radius:5px; padding:16px;" align="center">
-							<img src="img/<?php echo $row["product_image"]; ?>" class="img-responsive" /><br />
+						<h4 class="text-info"><?php echo $row["product_name"]; ?></h4>
 
-							<h4 class="text-info"><?php echo $row["product_name"]; ?></h4>
+						<h4 class="text-danger">$ <?php echo $row["product_price"]; ?></h4>
 
-							<h4 class="text-danger">$ <?php echo $row["product_price"]; ?></h4>
+						
 
-							<input type="submit" name="add_to_wishlist" style="margin-top:5px;" class="btn btn-success" value="Add to Wishlist" />
-<?php
-                 
-?>
-						</div>
-					</form>
-				</div>
-		<?php
-			}
-		}
-		?>
+						<input type="hidden" name="hidden_name" value="<?php echo $row["product_name"]; ?>" />
 
-		<div style="clear:both"></div>
-		<br />
-		<h3>Order Details</h3>
-		<div class="table-responsive">
-			<table class="table table-bordered">
-				<tr>
-					<th width="40%">Product Name</th>
+						<input type="hidden" name="hidden_price" value="<?php echo $row["product_price"]; ?>" />
 
-					<th width="20%">Price</th>
-					
-					<th width="5%">Action</th>
-				</tr>
+						<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Wishlist" />
 
-
-
-			</table>
+					</div>
+				</form>
+			</div>
+			<?php
+					}
+				}
+			?>
+			<div style="clear:both"></div>
+			<br />
+			<h3>Order Details</h3>
+			<div class="table-responsive">
+				<table class="table table-bordered">
+					<tr>
+						<th width="40%">Item Name</th>
+						
+						<th width="20%">Price</th>
+						
+						<th width="5%">Action</th>
+					</tr>
+					<?php
+					if(!empty($_SESSION["shopping_cart"]))
+					{
+						$total = 0;
+						foreach($_SESSION["shopping_cart"] as $keys => $values)
+						{
+					?>
+					<tr>
+						<td><?php echo $values["product_name"]; ?></td>	
+						<td>$ <?php echo $values["product_price"]; ?></td>
+						
+						<td><a href="cart.php?action=delete&id=<?php echo $values["product_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+					</tr>
+					<?php
+							$total = $total + ( $values["product_price"]);
+						}
+					?>
+					<tr>
+						<td colspan="3" align="right">Total</td>
+						<td align="right">$ <?php echo number_format($total, 2); ?></td>
+						<td></td>
+					</tr>
+					<?php
+					}
+					?>
+						
+				</table>
+			</div>
 
 			<div class="col-sm-12 col-md-6 text-right">
 					<a href="user.php" class="btn btn-md btn-block btn-success text-uppercase"> Back </a>
 					</div>
-			</div>
+
+			
+		</div>
 	</div>
-	
-</body>
+	<br />
+	</body>
 </html>
